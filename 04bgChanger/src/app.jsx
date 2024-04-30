00f8ea3439360b0202f629047c17cd1b74fb6556
ABHAY <369abhayg@gmail.com>
2024-04-30 11:35:59 +0530

    updated 04bgChanger

--- 04bgChanger/src/app.jsx
+++ 04bgChanger/src/app.jsx
@@ -1,24 +1,22 @@
 import { useState } from "react";
-import './app.css'
 
 export function App() {
   const [color, setColor] = useState('olive')
 
   return (
-   <div className="w-full h-screen " style={{backgroundColor:color}} >
-    <div className="flex flex-wrap justify-center bottom-12 inset-x-0 inset-y-0">
-      <p>
-      <button style={{backgroundColor:'red', padding:4, outline:'none', borderRadius:'5px', margin:'10px',color:'white', fontWeight:600}} onClick={()=>setColor('red')}>RED</button>
-      <button style={{backgroundColor:'green', padding:4, outline:'none', borderRadius:'5px', margin:'10px', fontWeight:600, color:'white'}}  onClick={()=>setColor('green')}>GREEN</button>
-      <button style={{backgroundColor:'blue', padding:4, outline:'none', borderRadius:'5px', margin:'10px', fontWeight:600, color:'white'}} onClick={()=>setColor('blue')}>BLUE</button>
-      <button style={{backgroundColor:'yellow', padding:4, outline:'none', borderRadius:'5px', margin:'10px', fontWeight:600, color:'black'}} onClick={()=>setColor('yellow')}>YELLOW</button>
-      <button style={{backgroundColor:'purple', padding:4, outline:'none', borderRadius:'5px', margin:'10px', fontWeight:600, color:'white'}} onClick={()=>setColor('purple')}>PURPLE</button>
-      <button style={{backgroundColor:'orange', padding:4, outline:'none', borderRadius:'5px', margin:'10px', fontWeight:600, color:'white'}} onClick={()=>setColor('orange')}>ORANGE</button>
-      <button style={{backgroundColor:'brown', padding:4, outline:'none', borderRadius:'5px', margin:'10px', fontWeight:600, color:'white'}} onClick={()=>setColor('brown')}>BROWN</button>
-      <button style={{backgroundColor:'lavender', padding:4, outline:'none', borderRadius:'5px', margin:'10px', fontWeight:600}} onClick={()=>setColor('lavender')}>LAVENDER</button>
-      <button style={{backgroundColor:'white', padding:4, outline:'none', borderRadius:'5px', margin:'10px', fontWeight:600}} onClick={()=>setColor('white')}>WHITE</button>
-      <button style={{backgroundColor:'black', padding:4, outline:'none', borderRadius:'5px', margin:'10px', fontWeight:600, color:'white'}} onClick={()=>setColor('black')}>BLACK</button>
-      </p>
+   <div className="w-full h-screen flex justify-center" style={{backgroundColor:color}}>
+    <div className="px-4 py-3 gap-2 fixed bottom-10 rounded-xl flex flex-wrap bg-white ">
+      <button className="font-bold py-2 rounded-xl px-3 text-white" style={{backgroundColor:'red'}} onClick={()=>{setColor('red')}}>Red</button>
+      <button className="font-bold p-2 ml-2 rounded-xl px-3 text-white" style={{backgroundColor:'green'}} onClick={()=>{setColor('green')}}>Green</button>
+      <button className="font-bold p-2 ml-2 rounded-xl px-3" style={{backgroundColor:'yellow'}} onClick={()=>{setColor('yellow')}}>Yellow</button>
+      <button className="font-bold p-2 ml-2 rounded-xl px-3 text-white" style={{backgroundColor:'blue'}} onClick={()=>{setColor('blue')}}>Blue</button>
+      <button className="font-bold p-2 ml-2 rounded-xl px-3" style={{backgroundColor:'lavender'}} onClick={()=>{setColor('lavender')}}>Lavender</button>
+      <button className="font-bold p-2 ml-2 rounded-xl px-3 text-white" style={{backgroundColor:'brown'}} onClick={()=>{setColor('brown')}}>Brown</button>
+      <button className="font-bold p-2 ml-2 rounded-xl px-3" style={{backgroundColor:'orange'}} onClick={()=>{setColor('orange')}}>Orange</button>
+      <button className="font-bold p-2 ml-2 rounded-xl px-3 text-white" style={{backgroundColor:'black'}} onClick={()=>{setColor('black')}}>Black</button>
+      <button className="font-bold p-2 ml-2 rounded-xl px-3" style={{backgroundColor:'white'}} onClick={()=>{setColor('white')}}>White</button>
+      <button className="font-bold p-2 ml-2 rounded-xl px-3 text-white" style={{backgroundColor:'purple'}} onClick={()=>{setColor('purple')}}>Purple</button>
+      
     </div>
    </div>
   )
