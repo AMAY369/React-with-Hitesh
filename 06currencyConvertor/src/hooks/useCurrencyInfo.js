ba54926d45a6e66c680cb1fffb59d1f6aa3caa77
ABHAY <369abhayg@gmail.com>
2024-05-20 14:22:55 +0530

    created currency convertor

---
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import {useState, useEffect} from "react"
+
+function useCurrencyInfo(currency){
+  const [data, setData ]  = useState({})
+  useEffect(()=>{
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.min.json`)
+    .then((res) => res.json())
+    .then((res)=>setData(res[currency]))
+  },[currency])
+
+  return data;
+}
+
+export default useCurrencyInfo;
